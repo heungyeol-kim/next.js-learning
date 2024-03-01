c3694975ea81a191d7b7469f4ddc8e11b3539922
heungyeol-kim <heungyeol.kim@musinsa.com>
2024-03-01 18:19:13 +0900

    2.8 Metadata

--- learn-nextjs14/app/not-found.tsx
+++ learn-nextjs14/app/not-found.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+};
+
 export default function NotFound() {
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
+      <h1>Not Found!</h1>
     </div>
   );
 }
